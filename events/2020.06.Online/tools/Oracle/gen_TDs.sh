b7541ef4abdca55468ca4650c31ce50b6e6559eb
Michael Lagally <Michael.Lagally@oracle.com>
2020-06-16 11:48:35 +0200

    device ID update

--- events/2020.06.Online/tools/Oracle/gen_TDs.sh
+++ events/2020.06.Online/tools/Oracle/gen_TDs.sh
@@ -15,7 +15,7 @@ node dm2td.js ../../TDs/Oracle/DMs/ora_obd2_device_model.json $IOTCS 0-AB 1B70FB
 node dm2td.js ../../TDs/Oracle/DMs/Festo.json $IOTCS 0-AB 290A9892-8400-4FA7-9874-5FE4DB12DE81 > ../../TDs/Oracle/Festo_Shared.jsonld
 
 # LED
-node dm2td.js ../../TDs/Oracle/DMs/LED.json $IOTCS 0-AB F6A7936A-D582-472F-938D-9B94DD1FC109 > ../../TDs/Oracle/LED_Siemens.jsonld
+node dm2td.js ../../TDs/Oracle/DMs/LED.json $IOTCS 0-AB B7988C38-E3AD-47B7-B89D-9A451BF3CFAB > ../../TDs/Oracle/LED_Siemens.jsonld
 
 # Connected Car
 # node dm2td.js ../../TDs/Oracle/DMs/Connected_Car.json w3ctest.iadstg.iot.ocs.oraclecloud.com 0-AB F846B3B1-1154-4721-8790-1586DC802366 > ../../TDs/Oracle/Connected_Car_Shared.jsonld
