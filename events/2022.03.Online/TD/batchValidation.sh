31bec1b808510c91c8aad4c21544b6b00d2f57ac
Ege Korkan <egekorkan@gmail.com>
2022-04-27 15:33:58 +0200

    fix some small errors and remove intermediary files

--- events/2022.03.Online/TD/batchValidation.sh
+++ events/2022.03.Online/TD/batchValidation.sh
@@ -1,3 +1,5 @@
-node ~/git/thingweb-playground/packages/cli/index.js -t TM -i Ditto/TMs/ -o Results/ditto2 -a -m Ditto/ditto.csv
-node ~/git/thingweb-playground/packages/cli/index.js -t TD -i Ditto/TDs/ -o Results/ditto1 -a -m Ditto/ditto.csv
-node ~/git/thingweb-playground/packages/cli/index.js --merge-only Results/ditto1.csv Results/ditto2.csv -o Results/ditto
+node ~/git/thingweb-playground/packages/cli/index.js -t TM -i Ditto/TMs/ -o Results/dittoTM -a -m Ditto/ditto.csv
+node ~/git/thingweb-playground/packages/cli/index.js -t TD -i Ditto/TDs/ -o Results/dittoTD -a -m Ditto/ditto.csv
+node ~/git/thingweb-playground/packages/cli/index.js --merge-only Results/dittoTM.csv Results/dittoTD.csv -o Results/ditto
+rm Results/dittoTM.csv
+rm Results/dittoTD.csv
