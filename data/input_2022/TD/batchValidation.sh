8795f448c2c83fa1bab1b93b877b504c803784d8
Michael McCool <michael.mccool@intel.com>
2022-05-18 13:46:18 -0300

    intel proxy, relative paths in script

--- data/input_2022/TD/batchValidation.sh
+++ data/input_2022/TD/batchValidation.sh
@@ -4,6 +4,7 @@ PLAYGROUNDLOCATION=~/Dev/thingweb-playground/
 
 # Ditto
 IMPLEMENTATION=Ditto
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TM -i $IMPLEMENTATION/TMs/ -o Results/$IMPLEMENTATION-TM -a -m $IMPLEMENTATION/$IMPLEMENTATION.csv
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION-TD -a -m $IMPLEMENTATION/$IMPLEMENTATION.csv
 node $PLAYGROUNDLOCATION/packages/cli/index.js --merge-only Results/$IMPLEMENTATION-TM.csv Results/$IMPLEMENTATION-TD.csv -o Results/$IMPLEMENTATION
@@ -12,6 +13,7 @@ rm Results/$IMPLEMENTATION-TD.csv
 
 # ECLASS
 IMPLEMENTATION=ECLASS
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # EDITDOR
@@ -23,77 +25,97 @@ node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o 
 # rm Results/$IMPLEMENTATION-TM.csv
 # rm Results/$IMPLEMENTATION-TD.csv
 
-# # flask-tum
+# flask-tum
 IMPLEMENTATION=flask-tum
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # Fujitsu
 IMPLEMENTATION=Fujitsu
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # hitachi-esp-idf
 IMPLEMENTATION=hitachi-esp-idf
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # hitachi-node-red
 IMPLEMENTATION=hitachi-node-red
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # intel-nodejs
 IMPLEMENTATION=intel-nodejs
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # intel-wot-ha
 IMPLEMENTATION=intel-wot-ha
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # intel-proxy
-# only a csv file?
+# only a csv file - manual results only
+IMPLEMENTATION=intel-proxy
+echo "$IMPLEMENTATION"
+cp $IMPLEMENTATION/$IMPLEMENTATION.csv Results/$IMPLEMENTATION.csv
 
 # manual tms from TUM
 IMPLEMENTATION=manual-tms
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TM -i $IMPLEMENTATION/TMs/ -o Results/$IMPLEMENTATION -a
 
 # NHK
 IMPLEMENTATION=NHK
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # node-wot
 IMPLEMENTATION=node-wot
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # Oracle
 IMPLEMENTATION=Oracle
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # Philips Hue
 IMPLEMENTATION=philips-hue
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # saywot
 IMPLEMENTATION=saywot
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a -m $IMPLEMENTATION/$IMPLEMENTATION.csv
 
 # WebThings
 IMPLEMENTATION=WebThings
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # Logilabs (TDD)
 IMPLEMENTATION=logilab
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # TinyIoT (TDD)
 IMPLEMENTATION=TinyIoT
+echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 ## Older
 
 # Unibo Farm
 IMPLEMENTATION=unibo-farm
-IMPLEMENTATION_DIR=~/Dev/wot-testing/data/input_2021/TD/TDs/AllOrgs/$IMPLEMENTATION
+IMPLEMENTATION_DIR=../../input_2021/TD/TDs/AllOrgs/$IMPLEMENTATION
+echo "$IMPLEMENTATION in $IMPLEMENTATION_DIR"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION_DIR/ -o Results/$IMPLEMENTATION -a
 
 # ECHONET Bridge
 IMPLEMENTATION=echonet-bridge
-IMPLEMENTATION_DIR=~/Dev/wot-testing/data/input_2021/TD/TDs/AllOrgs/$IMPLEMENTATION
+IMPLEMENTATION_DIR=../../input_2021/TD/TDs/AllOrgs/$IMPLEMENTATION
+echo "$IMPLEMENTATION in $IMPLEMENTATION_DIR"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION_DIR/ -o Results/$IMPLEMENTATION -a
