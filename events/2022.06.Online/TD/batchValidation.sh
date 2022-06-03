ff2e6ea3c8edba2cb1bd2f293dc5429cc4210cb5
Michael McCool <michael.mccool@intel.com>
2022-06-03 14:30:27 -0300

    manual.csv for intel-wot-ha

--- events/2022.06.Online/TD/batchValidation.sh
+++ events/2022.06.Online/TD/batchValidation.sh
@@ -26,8 +26,8 @@ echo "$IMPLEMENTATION"
 node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a -m $IMPLEMENTATION/TDs/manual.csv
 
 # intel-proxy
-# JUST a manual.csv, no TDs, so just copy it over, 
-# ... but make sure to name after the implementation
+# JUST a manual.csv, no TDs, so just copy it over, already named after implementation
 IMPLEMENTATION=intel-proxy
 echo "$IMPLEMENTATION"
 cp $IMPLEMENTATION/$IMPLEMENTATION.csv Results/$IMPLEMENTATION.csv
+
