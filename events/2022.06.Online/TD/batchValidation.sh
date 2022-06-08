78a6087de525592c11cf02696d9a30be0e70533c
Sebastian Kaebisch <sebastian.kaebisch@siemens.com>
2022-06-08 11:42:46 +0200

    batValidation.sh update

--- events/2022.06.Online/TD/batchValidation.sh
+++ events/2022.06.Online/TD/batchValidation.sh
@@ -1,33 +1,79 @@
 #!/bin/bash
 
-# Note: this needs to be updated for where you have installed thingweb-playground
-#PLAYGROUNDLOCATION=~/git/thingweb-playground/
-PLAYGROUNDLOCATION=~/Dev/thingweb-playground/
+PLAYGROUNDLOCATION=~/git/thingweb-playground/
 
 # Ditto
 IMPLEMENTATION=Ditto
-# node $PLAYGROUNDLOCATION/packages/cli/index.js -t TM -i $IMPLEMENTATION/TMs/ -o Results/$IMPLEMENTATION-TM -a -m $IMPLEMENTATION/$IMPLEMENTATION.csv
-# node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION-TD -a -m $IMPLEMENTATION/$IMPLEMENTATION.csv
-# node $PLAYGROUNDLOCATION/packages/cli/index.js --merge-only Results/$IMPLEMENTATION-TM.csv Results/$IMPLEMENTATION-TD.csv -o Results/$IMPLEMENTATION
-# rm Results/$IMPLEMENTATION-TM.csv
-# rm Results/$IMPLEMENTATION-TD.csv
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TM -i $IMPLEMENTATION/TMs/ -o Results/$IMPLEMENTATION-TM -a -m $IMPLEMENTATION/$IMPLEMENTATION.csv
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION-TD -a -m $IMPLEMENTATION/$IMPLEMENTATION.csv
+node $PLAYGROUNDLOCATION/packages/cli/index.js --merge-only Results/$IMPLEMENTATION-TM.csv Results/$IMPLEMENTATION-TD.csv -o Results/$IMPLEMENTATION
+rm Results/$IMPLEMENTATION-TM.csv
+rm Results/$IMPLEMENTATION-TD.csv
 
 # ECLASS
-# IMPLEMENTATION=ECLASS
-# node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+IMPLEMENTATION=ECLASS
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
-# saywot
-# IMPLEMENTATION=saywot
-# node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a -m $IMPLEMENTATION/$IMPLEMENTATION.csv
+# EDITDOR
+# ERRORS
+IMPLEMENTATION=editdor
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TM -i $IMPLEMENTATION/TMs/ -o Results/$IMPLEMENTATION-TM -a
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION-TD -a
+node $PLAYGROUNDLOCATION/packages/cli/index.js --merge-only Results/$IMPLEMENTATION-TM.csv Results/$IMPLEMENTATION-TD.csv -o Results/$IMPLEMENTATION
+rm Results/$IMPLEMENTATION-TM.csv
+rm Results/$IMPLEMENTATION-TD.csv
+
+# # flask-tum
+IMPLEMENTATION=flask-tum
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+
+# Fujitsu
+IMPLEMENTATION=Fujitsu
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+
+# hitachi-esp-idf
+IMPLEMENTATION=hitachi-esp-idf
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+
+# hitachi-node-red
+IMPLEMENTATION=hitachi-node-red
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # intel-nodejs
 IMPLEMENTATION=intel-nodejs
-echo "$IMPLEMENTATION"
-node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a -m $IMPLEMENTATION/TDs/manual.csv
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
 
 # intel-proxy
-# JUST a manual.csv, no TDs, so just copy it over, already named after implementation
-IMPLEMENTATION=intel-proxy
-echo "$IMPLEMENTATION"
-cp $IMPLEMENTATION/$IMPLEMENTATION.csv Results/$IMPLEMENTATION.csv
+# only a csv file?
 
+# manual tms from TUM
+IMPLEMENTATION=manual-tms
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TM -i $IMPLEMENTATION/TMs/ -o Results/$IMPLEMENTATION -a
+
+# NHK
+IMPLEMENTATION=NHK
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+
+# node-wot
+IMPLEMENTATION=node-wot
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+
+# Oracle
+IMPLEMENTATION=Oracle
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+
+# Philips Hue
+IMPLEMENTATION=philips-hue
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+
+# saywot
+IMPLEMENTATION=saywot
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a -m $IMPLEMENTATION/$IMPLEMENTATION.csv
+
+# WebThings
+IMPLEMENTATION=WebThings
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+
+# Logilabs (TDD)
+IMPLEMENTATION=logilab
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
