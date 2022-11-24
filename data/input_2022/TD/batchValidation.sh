3326ff014d5cade12f4d889af41785a67bcedbc3
Ege Korkan <egekorkan@gmail.com>
2022-11-24 22:19:33 +0100

    update batch validation

--- data/input_2022/TD/batchValidation.sh
+++ data/input_2022/TD/batchValidation.sh
@@ -30,7 +30,7 @@ rm Results/$IMPLEMENTATION-TD.csv
 # flask-tum
 IMPLEMENTATION=flask-tum
 echo "$IMPLEMENTATION"
-node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TD -i $IMPLEMENTATION/TDs/ -o Results/$IMPLEMENTATION -a -m $IMPLEMENTATION/TDs/manual.csv
 
 # Fujitsu
 IMPLEMENTATION=Fujitsu
@@ -84,8 +84,7 @@ cp $IMPLEMENTATION/$IMPLEMENTATION.csv Results/$IMPLEMENTATION.csv
 # Fischertechnik Factory Simulator
 IMPLEMENTATION=fischertechnik-factory-simulator
 echo "$IMPLEMENTATION"
-node $PLAYGROUNDLOCATION/packages/cli/index.js -t TM -i $IMPLEMENTATION/TMs/ -o Results/$IMPLEMENTATION -a
-
+node $PLAYGROUNDLOCATION/packages/cli/index.js -t TM -i $IMPLEMENTATION/TMs/ -o Results/$IMPLEMENTATION -a -m $IMPLEMENTATION/TMs/manual.csv
 # NHK
 IMPLEMENTATION=NHK
 echo "$IMPLEMENTATION"
